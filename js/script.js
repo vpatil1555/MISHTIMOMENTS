efe878ba073b76b6aec0a7942e970a795c9cf2ea
vpatil1555 <73878432+vpatil1555@users.noreply.github.com>
2023-05-29 13:00:50 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function imgSlider(anything){
+  document.querySelector('.starbucks').src = anything;
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+// sticky header
+window.onscroll = function() {myFunction()};
+
+// get header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if(window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+
+  }else {
+    header.classList.remove("sticky");
+  }
+}
